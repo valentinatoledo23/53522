4872e90cb7068788e4981716f75926490e96dd11
Valentina Toledo <valentina.toledo.utn@gmail.com>
2026-05-18 01:03:54 -0300

    Primer commit

--- CustomCalculatorListener.js
+++
@@ -1,9 +0,0 @@
-import CalculatorListener from "./generated/CalculatorListener.js";
-
-export class CustomCalculatorListener extends CalculatorListener {
-
-    enterStat(ctx) {
-        console.log(`Se detectó una: ${ctx.constructor.name}`);
-    }
-
-}
